850e3c85f9a2d67937bd8ef6280fb6fa6f479881
Mahendra Chaurasia1 <mahendrachaurasia@in.ibm.com>
2021-09-12 21:31:55 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ weatherForm.addEventListener("submit",(event)=>{
     msg2.textContent=""
     const location = searchLocation.value
 
-    fetch("http://localhost:3000/weather?address="+location).then((response)=>{
+    fetch("/weather?address="+location).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
               // console.log(data.error)
--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require("express")
 const hbs=require("hbs")
 
 const app = express()
+const port = process.env.PORT || 3000
 //express configs
 const publicDirPath = path.join(__dirname,"../public")
 const viewsPath=path.join(__dirname,"../templates/views")
@@ -79,6 +80,6 @@ app.get("*",(req,res)=>{
     errorMsg: "Page NOT found"
   })
 })
-app.listen(3000,()=>{
-  console.log("server is up on port 3000")
+app.listen(port,()=>{
+  console.log("server is up on port "+port)
 })
